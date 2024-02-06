ae36d6f07a72bc15bbcf3256ac2762a84eb60ce3
induchennupati <induchennupati7@gmail.com>
2024-02-06 16:06:40 +0530

    04/02/2024

--- fastapienv/books.py
+++ fastapienv/books.py
@@ -19,12 +19,14 @@ async def read_all_books():
 async def read_all_books():
     return {'book-title':'my fav book!'}
 
+#path parameter
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold()==book_title.casefold():
             return book
 
+#query parameter
 @app.get("/books/")      
 async def read_category_by_query(category: str):
     books_to_return=[]
@@ -33,3 +35,13 @@ async def read_category_by_query(category: str):
             books_to_return.append(book)
     return books_to_return
 
+
+@app.get("/book/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return=[]
+    for book in BOOKS:
+        if book.get('author').casefold()==book_author.casefold() and \
+            book.get('category').casefold==category.casefold():
+            books_to_return.append(book)
+                
+    return books_to_return
